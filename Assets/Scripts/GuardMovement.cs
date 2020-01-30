10bb7d136266512b7280671f03475ea36442bb08
thetiger756 <46494603+thetiger756@users.noreply.github.com>
2020-01-30 15:46:23 -0500

    v1.1
    PlayerMovement script and MouseLook script should both be totally done. Gravity is totally done. Jumping totally done. NavMesh implemented. GuardMovement Script started.

---
+++ Assets/Scripts/GuardMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardMovement : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent;
+    Vector3 target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            //find the players position
+            target = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+            agent.SetDestination(target);
+        }
+    }
+}
